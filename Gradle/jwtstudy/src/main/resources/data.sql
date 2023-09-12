628cc10c7d083a0618847a9aae77e6193457d8ab
heecheol shin <singun41@gmail.com>
2023-09-12 22:20:57 +0900

    update

--- Gradle/jwtstudy/src/main/resources/data.sql
+++ Gradle/jwtstudy/src/main/resources/data.sql
@@ -2,21 +2,21 @@ USE study;
 
 -- Jasypt : asdf
 -- INSERT INTO tbl_member (c_uuid, c_user_id, c_password, c_enabled)
--- VALUES (ordered_uuid(), 'developer', 'SYWGhrR/Hx0jPABjKfi6Dw==', 1);
+-- VALUES (ordered_uuid(), 'admin', 'SYWGhrR/Hx0jPABjKfi6Dw==', 1);
 
 -- Bcrypt : asdf
 INSERT INTO tbl_member (c_uuid, c_user_id, c_password, c_enabled)
-VALUES (ordered_uuid(), 'developer', '$2a$10$eEVWAZ7A7aXmT4EZtKam3.KrVqjDJC3XpylLq6hp/QK7wFjfX7CoG', 1);
+VALUES (ordered_uuid(), 'admin', '$2a$10$eEVWAZ7A7aXmT4EZtKam3.KrVqjDJC3XpylLq6hp/QK7wFjfX7CoG', 1);
 
 INSERT INTO tbl_member_role
 SELECT a.c_uuid, 'ADMIN' 
 FROM tbl_member a
-WHERE a.c_user_id = 'developer';
+WHERE a.c_user_id = 'admin';
 
 INSERT INTO tbl_member_role
 SELECT a.c_uuid, 'USER' 
 FROM tbl_member a
-WHERE a.c_user_id = 'developer';
+WHERE a.c_user_id = 'admin';
 
 
 
