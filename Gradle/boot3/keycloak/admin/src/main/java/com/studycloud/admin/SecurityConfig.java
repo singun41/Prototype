e0d21bacbd70f79dbf1ae2c525bdfadc35592531
heecheol shin <singun41@gmail.com>
2023-11-25 13:01:55 +0900

    fix: https mixed content error 수정

--- Gradle/boot3/keycloak/admin/src/main/java/com/studycloud/admin/SecurityConfig.java
+++ Gradle/boot3/keycloak/admin/src/main/java/com/studycloud/admin/SecurityConfig.java
@@ -17,6 +17,15 @@ public class SecurityConfig {
       .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
       .anyRequest().authenticated()
     )
+
+    /*
+      spring boot admin 자체에서 js나 css를 제공할 때 http로 전달하기 때문에 https 환경인 경우
+      브라우저의 콘솔에서 mixed content 경고가 나오면서 접속이 되지 않는다.
+      해결하기 위해선 아래 헤더를 추가해줘야 한다.
+      참고: https://stackoverflow.com/questions/72035591/spring-boot-admin-mixed-content-and-not-show-styles-in-server-https
+    */
+    .headers(t -> t.contentSecurityPolicy(x -> x.policyDirectives("upgrade-insecure-requests")))
+
     .formLogin(t -> t.loginPage("/login").permitAll().defaultSuccessUrl("/applications"))
     .logout(t -> t.logoutUrl("/logout"))
     .sessionManagement(t -> t.invalidSessionUrl("/login"))
