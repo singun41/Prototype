c6d326a9b386407900b1f89b3235359043fe0282
heecheol shin <singun41@gmail.com>
2024-02-03 16:12:29 +0900

    fix: server description 수정

--- Gradle/boot3/keycloak/user/src/main/java/com/studykeycloak/user/UserApplication.java
+++ Gradle/boot3/keycloak/user/src/main/java/com/studykeycloak/user/UserApplication.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @EnableWebSecurity
-@OpenAPIDefinition(servers = @Server(url = "http://localhost:8080/service-user", description = "Default Server URL"))   // prod 환경에서는 해당 URL을 입력, gateway에서 predicates로 처리하는 접미사 url /service-user 추가해줘야 함.
+@OpenAPIDefinition(servers = @Server(url = "http://localhost:8080/service-user", description = "Service-User"))   // prod 환경에서는 해당 URL을 입력, gateway에서 predicates로 처리하는 접미사 url /service-user 추가해줘야 함.
 public class UserApplication {
 
 	public static void main(String[] args) {
