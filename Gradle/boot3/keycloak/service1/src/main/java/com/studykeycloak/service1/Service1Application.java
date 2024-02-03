4b0c1547a2e6432febaf9dcb99daf4896e1e41b8
heecheol shin <singun41@gmail.com>
2024-02-03 16:15:12 +0900

    fix: server description 수정

--- Gradle/boot3/keycloak/service1/src/main/java/com/studykeycloak/service1/Service1Application.java
+++ Gradle/boot3/keycloak/service1/src/main/java/com/studykeycloak/service1/Service1Application.java
@@ -13,7 +13,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 @EnableDiscoveryClient
-@OpenAPIDefinition(servers = @Server(url = "http://localhost:8080/service-1", description = "Default Server URL"))   // prod 환경에서는 해당 URL을 입력, gateway에서 predicates로 처리하는 접미사 url /service-1 추가해줘야 함.
+@OpenAPIDefinition(servers = @Server(url = "http://localhost:8080/service-1", description = "Service-1"))   // prod 환경에서는 해당 URL을 입력, gateway에서 predicates로 처리하는 접미사 url /service-1 추가해줘야 함.
 public class Service1Application {
 
 	public static void main(String[] args) {
